ec15c21c305af059f11a9117509215111eee3261
JHTNT <jerry1106666@gmail.com>
2019-06-30 23:35:28 +0800

    6/30

---
+++ 基礎題庫/a095. 麥哲倫的陰謀.py
@@ -0,0 +1,9 @@
+try:
+    while True:
+        x, y = map(int, input().split())
+        if x > y:
+            print(y + 1)
+        else:
+            print(y)
+except EOFError:
+    pass
---
+++ 基礎題庫/a104. 排序.py
@@ -0,0 +1,8 @@
+try:
+    while True:
+        n = input()
+        x = list(map(int, input().split()))
+        x.sort()
+        print(*x, sep=" ")
+except EOFError:
+    pass
