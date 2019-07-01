2878d8cc73b8853ecf78f5a3da1583419b01fead
JHTNT <jerry1106666@gmail.com>
2019-07-01 20:20:54 +0800

    2019/7/1

---
+++ 基礎題庫/a263. 日期差幾天.py
@@ -0,0 +1,12 @@
+import time
+import datetime
+try:
+    while True:
+        x1, y1, z1 = map(int, input().split())
+        x2, y2, z2 = map(int, input().split())
+        day1 = datetime.date(x1, y1, z1)
+        day2 = datetime.date(x2, y2, z2)
+        result = day2 - day1
+        print(str(abs(result.days)))
+except EOFError:
+    pass
