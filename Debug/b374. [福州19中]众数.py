6a1f0ed8e07b6e91ca7356a946d6a6ede6aa1dfa
JHTNT <jerry1106666@gmail.com>
2019-07-06 05:19:37 +0800

    2019/7/5

---
+++ Debug/b374. [福州19中]众数.py
@@ -0,0 +1,23 @@
+try:
+    def count_num(l):
+        count = {}
+        for i in l:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        max_appear = 0
+        for v in count.values():
+            if v > max_appear:
+                max_appear = v
+        if max_appear == 1:
+            return
+        for k, v in count.items():
+            if v == max_appear:
+                print(str(k) + " " + str(v))
+    while True:
+        n = int(input())
+        l = list(map(int, input().split()))
+        count_num(l)
+except EOFError:
+    pass
