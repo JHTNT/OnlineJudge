d561501f2503d45a5ed29fa653b5e83a1a62456b
JHTNT <jerry1106666@gmail.com>
2021-09-18 01:53:41 +0800

    Add

---
+++ LeetCode/18. 4Sum.py
@@ -0,0 +1,40 @@
+from sys import stdin
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        result = []
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total > target:
+                        right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+        return result
+
+def main():
+    for line in stdin:
+        nums = list(map(int, line.split()))
+        target = int(stdin.readline())
+        print(Solution().fourSum(nums, target))
+
+if __name__ == '__main__':
+    main()
